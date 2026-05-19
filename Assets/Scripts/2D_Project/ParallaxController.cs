64fc695aff71c2c50959fc384c66b3d0eb3eb219
Taeyoung Lee <scammerlee1943@gmail.com>
2026-05-19 19:22:07 +0900

    [Update] 패럴랙스 배경 스크롤링 구현

---
+++ Assets/Scripts/2D_Project/ParallaxController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class ParallaxController : MonoBehaviour
+{
+    [SerializeField] private Transform Transform_Camera;
+    [SerializeField] private ParallaxLayer[] _layers;
+
+    private float _previousCameraY;
+
+    private void Start()
+    {
+        _previousCameraY = Transform_Camera.position.y;
+    }
+
+    private void LateUpdate()
+    {
+        float deltaY = Transform_Camera.position.y - _previousCameraY;
+
+        foreach (var layer in _layers)
+        {
+            layer.MoveLayer(deltaY);
+        }
+
+        _previousCameraY = Transform_Camera.position.y;
+    }
+}
+
+[System.Serializable]
+public class ParallaxLayer
+{
+    public Transform Transform_Layer;
+    [Range(0f, 1f)] public float _parallaxSpeed = 0.5f;
+
+    public void MoveLayer(float deltaY)
+    {
+        Transform_Layer.position += new Vector3(0f, deltaY * _parallaxSpeed, 0f);
+    }
+}

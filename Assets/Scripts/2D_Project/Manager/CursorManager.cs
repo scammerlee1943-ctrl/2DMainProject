067c84d2fc6459c212b7538ae0bf13fd907b5deb
Taeyoung Lee <scammerlee1943@gmail.com>
2026-05-23 16:24:59 +0900

    [Update] 커서 개선

--- Assets/Scripts/2D_Project/Manager/CursorManager.cs
+++ Assets/Scripts/2D_Project/Manager/CursorManager.cs
@@ -1,26 +1,14 @@
 ﻿using UnityEngine;
 
-public enum CursorAnimState
-{
-    None = 0,
-    Idle,
-    ClickDown,
-    ClickUp,
-    Hover
-}
-
-
 public class CursorManager : MonoBehaviour
 {
     [SerializeField] private RectTransform RectTransform_Cursor;
     [SerializeField] private Animator Animator_Cursor;
 
-    private CursorAnimState _currentAnimState;
-
     private void Awake()
     {
         Cursor.visible = false;
-        //진짜 커서를 숨기기
+        Cursor.lockState = CursorLockMode.None;
     }
 
     private void Update()
@@ -31,37 +19,22 @@ public class CursorManager : MonoBehaviour
 
     private void MoveCursor()
     {
+        if (RectTransform_Cursor == null) return;
         RectTransform_Cursor.position = Input.mousePosition;
     }
 
     private void CheckMouseClick()
     {
+        if (Animator_Cursor == null) return;
+
         if (Input.GetMouseButtonDown(0))
         {
-            SetState(CursorAnimState.ClickDown);
+            Animator_Cursor.SetBool("IsClick", true);
         }
-        if(Input.GetMouseButtonUp(0))
-        {
-            SetState(CursorAnimState.ClickUp);
-        }
-    }
 
-    private void SetState(CursorAnimState newstate)
-    {
-        _currentAnimState = newstate;
-        switch (_currentAnimState)
+        if (Input.GetMouseButtonUp(0))
         {
-            case CursorAnimState.Idle:
-                break;
-            case CursorAnimState.ClickDown:
-                Animator_Cursor.SetTrigger("ClickDown");
-                break;
-            case CursorAnimState.ClickUp:
-                Animator_Cursor.SetTrigger("ClickUp");
-                break;
-            case CursorAnimState.Hover:
-                Animator_Cursor.SetTrigger("Hover");
-                break;
+            Animator_Cursor.SetBool("IsClick", false);
         }
     }
 }
